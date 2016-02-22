655e993ff56df4e05b8cb36a9e9718e41c6bccac
Matthew Vanderhoff <h0ffline@Maedre.uncc.edu>
2016-02-22 11:04:55 -0500

    made printf example, then broke it
    used valgrind to see what happens in memory when printf tries to
    format uninitialized vars, and when no var is given.

---
+++ ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20;
+    int height = 77;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}
+
---
+++ ex4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20;
+    int height;
+
+    printf("I am %d years old.\n");
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}
+
