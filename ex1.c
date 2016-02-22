5933fe0ffa0fd4c6f1a12522876e4352a2600e8f
Matthew Vanderhoff <h0ffline@Maedre.uncc.edu>
2016-02-22 09:46:36 -0500

    added more puts

--- ex1.c
+++ ex1.c
@@ -3,6 +3,10 @@
 int main(int argc, char *argv[])
 {
     puts("Hello World.");
+    puts("this is c");
+    puts("it is fast");
+    puts("so fast");
+    puts("but dangerous");
 
     return 0;
 }
